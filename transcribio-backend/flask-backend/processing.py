c4092300b3458d2ec05caaaff651233d94402b04
Chau Vu <chv004@ucsd.edu>
2020-10-11 02:23:48 -0700

    Add speech to transcript conversion

--- transcribio-backend/flask-backend/processing.py
+++ transcribio-backend/flask-backend/processing.py
@@ -1,5 +1,7 @@
 import os
 import ffmpeg
+from google.cloud import speech
+
 
 def processVideo(videoFileName):
     return extractAudio(videoFileName)
@@ -15,6 +17,36 @@ def extractAudio(videoFileName):
     os.remove(videoFileName)
     return speechToText(audioOut)
 
-
 def speechToText(rawAudio):
-    return str(type(rawAudio))
+    client = speech.SpeechClient()
+    audio = speech.RecognitionAudio(content=rawAudio)
+    config = speech.RecognitionConfig(
+		encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+		sample_rate_hertz=16000,
+		language_code="en-US",
+		enable_word_time_offsets=True,
+	)
+
+	# Detects speech in the audio file
+    response = client.recognize(request={"config": config, "audio": audio})
+    transcriptionResults = {}
+    transcriptionResults['transcript'] = ""
+    words = {}
+    
+    for result in response.results:
+        alternative = result.alternatives[0]
+        transcriptionResults['transcript'] += alternative.transcript
+        for word_info in alternative.words:
+            word = word_info.word
+            start_time = word_info.start_time
+            if word not in words:
+                words[word] = []
+            words[word].append({"start_time" : start_time.seconds})
+        transcriptionResults['words'] = words
+        
+    data = {
+        "success": True, 
+        "result": transcriptionResults
+    }
+    
+    return data
